fd9482d77925a6897fad6c1bfb413eb672a8b2e6
Axel Hodler <axel@hodler.co>
2019-08-31 15:57:38 +0200

    simple callback based api

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const https = require('https')
+const http = require('http')
 
-https.get('https://httpbin.org/get', res => {
-  res.on('data', d => {
-    process.stdout.write(d)
+const fetchData = (callback) => {
+  https.get('https://httpbin.org/get', res => {
+    res.on('data', data => {
+      callback(data)
+    })
   })
+}
+
+const server = http.createServer((req,res) => {
+  const callback = (content) => {
+    res.write(content)
+    res.end()
+  }
+  res.statusCode = 200
+  fetchData(callback)
+})
+
+const port = 3000
+const hostname = '127.0.0.1'
+server.listen(port, hostname,()=>{
+  console.log(`Running at http://${hostname}:${port}/`)
 })
